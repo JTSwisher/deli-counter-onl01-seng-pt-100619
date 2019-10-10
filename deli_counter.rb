a1cf27ced8ec37052896bba21d5bba4ec92f8a92
Jeff Swisher <jefftswisher@gmail.com>
2019-10-10 01:11:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(deli)
       puts "There is nobody waiting to be served!"
     else 
       deli.each do |name|
-        puts "Currently serving #{name}."
+        puts "Currently serving #{name.shift}."
     end
   end 
 end
